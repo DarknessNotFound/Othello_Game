f5a4a2167266764e32194837eb733284c06171a5
DarknessNotFound <60402265+DarknessNotFound@users.noreply.github.com>
2021-05-28 16:26:17 -0500

    adding side banners

--- gen_board.js
+++ gen_board.js
@@ -3,7 +3,7 @@ for(var i = 0; i < 64; i++) {
   const space = document.createElement('div');
   space.id = 'space';
   space.classList.add('space');
-  space.innerHTML = i;
+  //space.innerHTML = i;
 
   board.appendChild(space);
 }

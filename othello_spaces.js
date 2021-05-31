3408fa6d1626394fe2c0432bc1e9171b079965b2
DarknessNotFound <60402265+DarknessNotFound@users.noreply.github.com>
2021-05-31 12:08:26 -0500

    basic prototype setup

---
+++ othello_spaces.js
@@ -0,0 +1,35 @@
+function Space(placed, b_space) {
+  this.placed = placed || false;
+  this.b_space = b_space || true;
+}
+Space.prototype.constructor = Space;
+
+function Board(side_len) {
+  Space.call(this);
+  this.side_len = side_len || 8;
+  this.board_size = side_len * side_len;
+  this.spaces = [];
+  
+  this.func_temp = function () {
+    console.log("HI");
+  }
+
+
+}
+
+Board.prototype.make_spaces = function() {
+  this.spaces = [];
+  for(let i = 0; i < this.board_size; i++) {
+    spaces.push(new Space());
+  }
+  return;
+}
+
+Board.prototype.constructor = Board;
+
+var main_board = new Board(8);
+
+console.log(main_board.spaces);
+console.log(main_board.board_size);
+console.log(main_board.placed);
+console.log(main_board.b_space);

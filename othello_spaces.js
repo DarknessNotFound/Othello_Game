67207a40181c9c926d8bb230f7242cc1e0d38b3b
DarknessNotFound <60402265+DarknessNotFound@users.noreply.github.com>
2021-05-31 19:05:05 -0500

    set_initial_board_state() and other functions

--- othello_spaces.js
+++ othello_spaces.js
@@ -11,29 +11,121 @@ function Board(side_len, b_turn) {
   this.board_size = side_len * side_len;
   this.spaces = [];
   this.b_turn = b_turn || true;
+  
+  //Alternating between black and white spaces (starting with black).
+  this.initialSpaces = [28, 27, 35, 36];
 
-  this.make_spaces = function() {
-    this.spaces = [];
-    for(let i = 0; i < this.board_size; i++) {
-      this.spaces.push(new Space(i));
-    }
-    return true;
-  }
-
-  this.place_disk = function (id) {
-    if(this.legalSpace(id)) {
-      this.spaces[id].placed = true;
-      this.spaces[id].b_space = this.b_turn;
-      this.flipDisksInLine(id);
-    }
-  }
+  this.set_initial_board_state();
+  var edge_spaces = getEdgeSpaces(this.side_len, this.side_len);
 
 }
 Board.prototype.constructor = Board;
 
-var main_board = new Board(8);
+function getEdgeSpaces(height, width) {
+  let edgeSpaces = {
+    up_edge: [],
+    low_edge: [],
+    left_edge: [],
+    right_edge: []
+  };
+  let total_spaces = height * width;
 
-console.log(main_board.spaces);
-console.log(main_board.board_size);
-console.log(main_board.placed);
-console.log(main_board.b_space);
+  //Up_edge spaces and lower edge spaces
+  for(let i = 0; i < width; i++) {
+    edgeSpaces.up_edge.push(i);
+    edgeSpaces.low_edge.push(i + (total_spaces - width));
+  }
+
+  for(let i = 0; i < height; i++) {
+    edgeSpaces.left_edge.push(i*width);
+    edgeSpaces.right_edge.push(i*width + width - 1);
+  }
+
+  return edgeSpaces;
+}
+
+var dir_ids = { //directional ids
+  up_left: 0,
+  up_mid: 1,
+  up_right: 2,
+  right_mid: 3,
+  low_right: 4,
+  low_mid: 5,
+  low_left: 6,
+  left_mid: 7
+};
+
+Board.prototype.legalSpace = function(id, direction) {
+  let legalSpace = true;
+  switch (direction) {
+    case dir_ids.up_left:
+      if (this.edgeSpaces.up_edge.includes(id) ||
+          this.edgeSpaces.left_edge.includes(id))
+        legalSpace = false;
+      break;
+
+    case dir_ids.up_mid:
+      if (this.edgeSpaces.up_edge.includes(id))
+        legalSpace = false;
+      break;
+
+    case dir_ids.up_right:
+      if (this.edgeSpaces.up_edge.includes(id) ||
+          this.edgeSpaces.right_edge.includes(id))
+        legalSpace = false;
+      break;
+
+    case dir_ids.right_mid:
+      if (this.edgeSpaces.right_edge.includes(id))
+        legalSpace = false;
+      break;
+
+    case dir_ids.low_right:
+      if (this.edgeSpaces.low_edge.includes(id) ||
+          this.edgeSpaces.right_edge.includes(id))
+        legalSpace = false;
+      break;
+
+    case dir_ids.low_mid:
+      if (this.edgeSpaces.low_edge.includes(id))
+        legalSpace = false;
+      break;
+
+    case dir_ids.low_left:
+      if (this.edgeSpaces.up_edge.includes(id) ||
+          this.edgeSpaces.left_edge.includes(id))
+        legalSpace = false;
+      break;
+
+    case dir_ids.left_mid:
+      if (this.edgeSpaces.left_edge.includes(id))
+        legalSpace = false;
+      break;
+  }
+  return legalSpace;
+}
+
+Board.prototype.set_initial_board_state = function() {
+  this.spaces = [];
+  for(let i = 0; i < this.board_size; i++) {
+    this.spaces.push(new Space(i));
+  }
+
+  this.initialSpaces.forEach((space, index) => {
+    this.spaces[space].placed = true;
+    this.spaces[space].b_side = ( (index % 2) === 0);
+  });
+
+  return true;
+}
+
+Board.prototype.place_disk = function (id) {
+  if(this.legalSpace(id)) {
+    this.spaces[id].placed = true;
+    this.spaces[id].b_space = this.b_turn;
+    this.flipDisksInLine(id);
+  }
+}
+
+
+var main_board = new Board(8);
